e5ff8cd3cf0ceff30671d7f8c085bf7562a3211b
Lucas gabriel neves da silva santos <dexdex2000@hotmail.com>
2023-09-14 14:00:42 -0300

    update

---
+++ padariaback/src/main/java/br/com/padaria/padariaback/PadariabackApplication.java
@@ -0,0 +1,13 @@
+package br.com.padaria.padariaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadariabackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadariabackApplication.class, args);
+	}
+
+}
---
+++ padariaback/src/test/java/br/com/padaria/padariaback/PadariabackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.padaria.padariaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadariabackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
